21ae9cbd4bce55f15ba379ddc86e8713f2f7101a
patricio <pato@gmail.com>
2018-10-26 11:38:55 -0300

    commit media

--- pagmisperris/views.py
+++ pagmisperris/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.utils import timezone
 from .forms import PostForm
+from .models import Post
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
