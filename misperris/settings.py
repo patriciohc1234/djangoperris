6eed3a2ff49294abbf9547f34d13640193759a57
patricio <pato@gmail.com>
2018-11-05 21:46:13 -0300

    commit pequeño

--- misperris/settings.py
+++ misperris/settings.py
@@ -122,4 +122,4 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-LOGIN_REDIRECT_URL = '/'
+LOGIN_REDIRECT_URL = 'login.html'
